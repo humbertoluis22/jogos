eb3b1262a74f08c8ed74143a9e02178b3459837a
humbertoluis22 <humbertoluis2013@gmail.com>
2023-06-22 12:52:28 -0300

    atualizando o arquivo jogos

--- jogos.py
+++ jogos.py
@@ -1,17 +1,22 @@
 import  forca
 import adivinhacao
 
-print("**********************************")
-print("**********escolha o jogo**********")
-print("**********************************")
+def escolher_jogos():
+    print("**********************************")
+    print("**********escolha o jogo**********")
+    print("**********************************")
 
-print("(1)adivinhação (2)forca")
+    print("(1)adivinhação (2)forca")
 
-jogo = int(input("qual jogo? : "))
+    jogo = int(input("qual jogo? : "))
 
-if(jogo == 1):
-    print("jogando adivinhação")
-    adivinhacao.jogar_adivinhacao()
-elif(jogo ==2):
-    print("jogando forca")
-    forca.jogar_forca()
+    if(jogo == 1):
+        print("jogando adivinhação")
+        adivinhacao.jogar_adivinhacao()
+    elif(jogo ==2):
+        print("jogando forca")
+        forca.jogar_forca()
+
+
+if(__name__ == "__main__"):
+    escolher_jogos()
